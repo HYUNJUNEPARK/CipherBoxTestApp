e1736f70c0915ba58916049c420dcd0a4b2101a7
HYUNJUNEPARK <june2ac4dev@gmail.com>
2022-07-01 14:50:41 +0900

    rough proto type 1

--- app/src/main/java/com/june/strongboxkey/activity/MainActivity.kt
+++ app/src/main/java/com/june/strongboxkey/activity/MainActivity.kt
@@ -1,31 +1,27 @@
 package com.june.strongboxkey.activity
 
-import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
-import android.util.Log
 import android.view.View
 import android.widget.Toast
-import androidx.databinding.DataBindingUtil
-import com.june.strongboxkey.R
+import androidx.appcompat.app.AppCompatActivity
 import com.june.strongboxkey.databinding.ActivityMainBinding
 import com.june.strongboxkey.model.KeyPairModel
 import com.june.strongboxkey.util.AESUtils
 import com.june.strongboxkey.util.KeyProvider
 
 class MainActivity : AppCompatActivity() {
-    private lateinit var binding: ActivityMainBinding
+    private val binding by lazy { ActivityMainBinding.inflate(layoutInflater) }
+
     private var keyPairA: KeyPairModel? = null
     private var keyPairB: KeyPairModel? = null
     private var sharedSecretKey: ByteArray? = null
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        binding = DataBindingUtil.setContentView(this, R.layout.activity_main)
-
-        binding.mainActivity = this
+        setContentView(binding.root)
     }
 
-    fun keyGen() {
+    fun keyGenButtonClicked(v: View) {
         keyPairA = KeyProvider().keyPair() //sender
         keyPairB = KeyProvider().keyPair() //recipient
 
@@ -36,11 +32,6 @@ class MainActivity : AppCompatActivity() {
             Toast.makeText(this, "Shared Secret Key 생성 실패", Toast.LENGTH_SHORT).show()
         }
         initKeyPairView()
-
-        //TODO
-//        val encryption = AESUtils().encryption("user Input", sharedSecretKey)
-//        val decryption = AESUtils().decryption(encryption, sharedSecretKey)
-//        Log.d("testLog", "DECRYPTION: $decryption")
     }
 
     private fun initKeyPairView() {
@@ -48,5 +39,20 @@ class MainActivity : AppCompatActivity() {
         if (keyPairA?.publicKey != null) binding.publicKeyAText.visibility = View.VISIBLE else binding.publicKeyAText.visibility = View.INVISIBLE
         if (keyPairB?.privateKey != null) binding.privateKeyBText.visibility = View.VISIBLE else binding.privateKeyBText.visibility = View.INVISIBLE
         if (keyPairB?.publicKey != null) binding.publicKeyBText.visibility = View.VISIBLE else binding.publicKeyBText.visibility = View.INVISIBLE
+    }
+
+    fun messageSendButtonClicked(v: View) = with(binding) {
+        if (keyPairA == null || keyPairB == null || sharedSecretKey == null) {
+            Toast.makeText(this@MainActivity, "암복호화 키 필요", Toast.LENGTH_SHORT).show()
+            return@with
+        }
+        val userInput = messageEditText.text.toString()
+        userMessageTextView.text = userInput
+
+        val encryption = AESUtils().encryption(userInput, sharedSecretKey!!)
+        binding.encryptionTextView.text = encryption.toString()
+
+        val decryption = AESUtils().decryption(encryption, sharedSecretKey!!)
+        binding.decryptionTextView.text = decryption
     }
 }
