4b0cdf7c504d1082917b98418d35a58cdaefd9f0
HYUNJUNEPARK <june2ac4dev@gmail.com>
2022-08-16 10:06:16 +0900

    renew ui

---
+++ app/src/main/java/com/june/strongboxkey/strongbox/ECKeyUtil.kt
@@ -0,0 +1,54 @@
+package com.june.strongboxkey.strongbox
+
+import java.math.BigInteger
+import java.security.KeyFactory
+import java.security.PublicKey
+import java.security.interfaces.ECPublicKey
+import java.security.spec.*
+
+class ECKeyUtil {
+    /**
+     * 서버에 업로드할 Public Key 를 x, y 좌표로 분해하는 메서드입니다.
+     *
+     * @param publicKey 서버에 업로드할 Public Key 를 파라미터로 전달합니다.
+     * @return x,y 좌표를 해시맵에 담아 반환합니다.
+     */
+    fun disassemble(publicKey: PublicKey): HashMap<String, String> {
+        val ecPublicKey = publicKey as ECPublicKey
+        return hashMapOf(
+            "affineX" to ecPublicKey.w.affineX.toString(),
+            "affineY" to ecPublicKey.w.affineY.toString()
+        )
+    }
+
+    /**
+     * 서버에서 받은 x, y 좌표를 Public Key 로 합성하는 메서드입니다.
+     *
+     * @param affineX 합성하려는 Public Key 의 x 좌표입니다.
+     * @param affineY 합성하려는 Public Key 의 y 좌표입니다.
+     * @return 합성한 Public Key 를 반환합니다.
+     */
+    fun assemble(affineX: String, affineY: String): PublicKey {
+        val affineX = BigInteger(affineX)
+        val affineY = BigInteger(affineY)
+        val ecPoint = ECPoint(affineX, affineY)
+        val keySpec = ECPublicKeySpec(ecPoint, ecParameterSpec())
+        val keyFactory = KeyFactory.getInstance("EC")
+        return keyFactory.generatePublic(keySpec)
+    }
+
+    //reference : Elliptic Curve Domain Parameters (https://www.secg.org/sec2-v2.pdf Page9 of 33-34)
+    private fun ecParameterSpec(): ECParameterSpec {
+        val p = BigInteger("FFFFFFFF00000001000000000000000000000000FFFFFFFFFFFFFFFFFFFFFFFF", 16)
+        val a = BigInteger("FFFFFFFF00000001000000000000000000000000FFFFFFFFFFFFFFFFFFFFFFFC", 16)
+        val b = BigInteger("5AC635D8AA3A93E7B3EBBD55769886BC651D06B0CC53B0F63BCE3C3E27D2604B", 16)
+        val gX = BigInteger("6B17D1F2E12C4247F8BCE6E563A440F277037D812DEB33A0F4A13945D898C296", 16)
+        val gY = BigInteger("4FE342E2FE1A7F9B8EE7EB4A7C0F9E162BCE33576B315ECECBB6406837BF51F5", 16)
+        val n = BigInteger("FFFFFFFF00000000FFFFFFFFFFFFFFFFBCE6FAADA7179E84F3B9CAC2FC632551", 16)
+        val h = 1
+        val ecField = ECFieldFp(p)
+        val curve = EllipticCurve(ecField, a, b)
+        val g = ECPoint(gX, gY)
+        return ECParameterSpec(curve, g, n, h)
+    }
+}
--- app/src/main/java/com/june/strongboxkey/strongbox/StrongBox.kt
+++ app/src/main/java/com/june/strongboxkey/strongbox/StrongBox.kt
@@ -18,19 +18,19 @@ import javax.crypto.spec.SecretKeySpec
 class StrongBox(private val context: Context) {
     //CBC(Cipher Block Chaining) Mode 에서 첫번째 암호문 대신 사용되는 IV(Initial Vector)로 0으로 초기화 되어있습니다.
     private val iv: ByteArray = ByteArray(16)
-    //기본 유형 키스토어(BKS)를 보관하고 있는 파일이름입니다.
+    //기본 유형 키스토어(BKS)를 보관하고 있는 파일 이름으로 해당 파일에는 패스워드가 걸려있습니다.
     private val keystoreFile = "default_keystore"
-    //기본 유형 키스토어(BKS)를 보관하고 있는 파일을 열기위한 패스워드입니다.
+    //기본 유형 키스토어(BKS)를 보관하고 있는 파일을 열기 위한 패스워드입니다.
     private val storePassword = "defaultStorePassword".toCharArray()
-    //기본 유형 키스토어(BKS)에서 보관하고 있는 sharedSecretKey 에 접근하기 위한 패스워드입니다.
+    //기본 유형 키스토어(BKS)에서 보관하고 있는 shared Secret Key 에 접근하기 위한 패스워드입니다.
     private val keyPassword = "defaultKeyPassword".toCharArray()
-    //AndroidKeyStore 에 저장되어 보관되고 있는 ECKeyPair 의 식별자입니다.
+    //안드로이드 키스토어(AndroidKeyStore) 에 저장되어있는 EC Key Pair 의 식별자입니다.
     private val keyAlias = "androidKeyStoreKey"
-    //AndroidKeyStore 로 해당 키스토어에는 사용자의 ECPrivateKey/ECPublicKey 가 저장되어 관리됩니다.
+    //안드로이드 키스토어(AndroidKeyStore) : 해당 키스토어에는 사용자의 EC Private Key / EC Public Key 가 저장되어 관리됩니다.
     private val androidKeyStore = KeyStore.getInstance("AndroidKeyStore").apply {
         load(null)
     }
-    //기본 유형 키스토어(BKS)로 패스워드가 필요한 파일에 저장되어 관리되고 해당 키스토어에서는 sharedSecretKey 가 저장되어 관리됩니다.
+    //기본 유형 키스토어(BKS) : 패스워드가 걸려있는 파일에 저장되고, 이 키스토어에서는 shared Secret Key 가 저장되어 관리됩니다.
     //File(PW : storePassword) -> keystore 접근 -> KeyEntry(PW: keyPassword) -> sharedSecretKey 접근
     private val defaultKeyStore = KeyStore.getInstance(KeyStore.getDefaultType()).apply {
         var fis: FileInputStream? = null
@@ -43,7 +43,7 @@ class StrongBox(private val context: Context) {
         load(fis, storePassword)
     }
     /**
-     * ECPrivateKey/ECPublicKey 를 생성하고 AndroidKeyStore 에 저장해주는 메서드입니다.
+     * EC Private Key/EC Public Key 를 생성하고 안드로이드 키스토어(AndroidKeyStore) 에 저장해주는 메서드입니다.
      * 안드로이드 API 31 이상 사용이 가능합니다.
      */
     fun generateECKeyPair() {
@@ -66,7 +66,7 @@ class StrongBox(private val context: Context) {
     }
 
     /**
-     * AndroidKeyStore 에 저장되어 있는 ECPublicKey 를 가져오는 메서드입니다.
+     * 안드로이드 키스토어(AndroidKeyStore) 에 저장되어 있는 EC Public Key 를 가져오는 메서드입니다.
      *
      * @return PublicKey
      */
@@ -76,12 +76,13 @@ class StrongBox(private val context: Context) {
 
     /**
      * 랜덤한 byteArray 를 생성해 주는 메서드입니다.
-     * 생성된 byteArray 사용처
-     * 1)해시를 만들 때 사용
-     * 2)keyId 로 사용
      *
-     * @param size 랜덤하게 생성될 byteArray 의 길이를 입력합니다.
-     * @return 랜덤하게 생성된 byteArray 를 String 타입으로 바꾼 뒤 반환합니다.
+     * 생성된 byteArray 사용처
+     * 1) 해시를 만들 때 사용
+     * 2) keyId 로 사용
+     *
+     * @param size byteArray 의 길이를 입력합니다.
+     * @return byteArray 를 String 타입으로 바꾼 뒤 반환합니다.
      */
     fun generateRandom(size: Int): String {
         val random = ByteArray(size).apply {
@@ -93,30 +94,28 @@ class StrongBox(private val context: Context) {
     }
 
     /**
-     * 나의 privateKey, 대화 상대의 publicKey 그리고 random byteArray 를 사용해 haredSecretKey 를 만들고
-     * 기본 유형 키스토어(KeyStore.getDefaultType()) 에 저장하는 메서드입니다.
+     * 나의 Private Key, 대화 상대의 Public Key 그리고 random byteArray 를 사용해 Shared Secret Key 를 만들고
+     * 이를 기본 유형 키스토어(defaultKeyStore, BKS) 에 저장하는 메서드입니다.
      *
-     * 채널을 초대한 사람이 해당 메서드를 사용할 때
+     * a. 채널을 초대한 사람이 해당 메서드를 사용할 때
      * -> keyId 를 채널의 메타데이터로 업로드하는 기능 구현이 필요
-     * -> 2) keyId 와 채널 URL 주소를 매핑해주는 로컬 DB 구현이 필요
-     * 채널 초대받은 사람이 해당 메서드를 사용할 떄
-     * -> 1) 채널 메타데이터로부터 데이터를 가져와 사용 파라미터로 nonce 로 사용
-     * -> 2) keyId 와 채널 URL 주소를 매핑해주는 로컬 DB 구현이 필요
+     * -> keyId 와 채널 URL 주소를 매핑해주는 로컬 DB 구현이 필요
      *
-     * @param publicKey 대화 상대의 publicKey 를 의미하며 서버로 부터 받아온 데이터를 사용합니다.
+     * b. 채널 초대받은 사람이 해당 메서드를 사용할 때
+     * -> 채널 메타데이터로부터 데이터를 가져와 사용 파라미터로 nonce 로 사용
+     * -> keyId 와 채널 URL 주소를 매핑해주는 로컬 DB 구현이 필요
+     *
+     * @param publicKey 대화 상대의 Public Key 를 의미하며, 서버로 부터 받아 온 데이터를 사용합니다.
      * @param nonce random byteArray 를 의미하며 해시를 만들 때 사용합니다.
-     * @return 문자열 타입의 keyId 를 반환하며 keyId 는 키스토어에서 sharedSecretKey 를 가져올 때 사용헙나다.
+     * @return 문자열 타입의 keyId 를 반환하며, keyId 는 키스토어에서 Shared Secret Key 를 가져올 때 사용헙나다.
      */
     fun generateSharedSecretKey(publicKey: PublicKey, nonce: String): String {
         val keyId:String = nonce
-
         val privateKey: PrivateKey
         androidKeyStore.getEntry(keyAlias, null).let { keyStoreEntry ->
             privateKey = (keyStoreEntry as KeyStore.PrivateKeyEntry).privateKey
         }
-
         val random:ByteArray = Base64.decode(nonce, Base64.DEFAULT)
-
         var sharedSecretKey: Key
         KeyAgreement.getInstance("ECDH").apply {
             init(privateKey)
@@ -128,18 +127,14 @@ class StrongBox(private val context: Context) {
             val hash = messageDigest.digest(random)
             sharedSecretKey = SecretKeySpec(hash, KeyProperties.KEY_ALGORITHM_AES)
         }
-        //TODO init 0000000 privatekey
-
-        /**
-         * 생성된 sharedSecretKey 를 기본 유형 키스토어(KeyStore.getDefaultType()) 에 비밀번화와 함께 저장하는 부분으로
-         * 기본 유형 키스토어는 파일로 관리되며 접근하기 위해서는 패스워드가 필요합니다.
-         * sharedSecretKey 를 키스토어에 저장 후 0 으로 채워진 무의미한 데이터로 덮어씌웁니다.
-         */
         defaultKeyStore.setKeyEntry(keyId, sharedSecretKey, keyPassword, null)
         sharedSecretKey = SecretKeySpec(ByteArray(16), KeyProperties.KEY_ALGORITHM_AES)
         val ksOut: FileOutputStream = context.openFileOutput(keystoreFile, Context.MODE_PRIVATE)
         defaultKeyStore.store(ksOut, storePassword)
         ksOut.close()
+
+        //TODO init 0000000 private key
+        //TODO init 0000000 shared secret key
 
         return keyId
     }
@@ -162,7 +157,6 @@ class StrongBox(private val context: Context) {
         return true
     }
 
-
     /**
      * 기본 유형 키스토어(BKS)에 저장되어 있는 SharedSecretKey 를 삭제하는 메서드입니다.
      *
@@ -178,7 +172,6 @@ class StrongBox(private val context: Context) {
         }
         return  true
     }
-
 
     /**
      * 기본 유형 키스토어(KeyStore.getDefaultType()) 에서 sharedSecretKey 를 가져와 원본 메시지를 암호화하는 메서드입니다.
